e154fb4ad0e40db584a8a27f4286e0aad7375e77
festradasolano <festradasolano@gmail.com>
2018-07-19 14:34:52 -0500

    Arguments for input/output files

--- nelly-ml/analysis/nelly-moa/src/main/java/co/edu/unicauca/dtm/nelly/MOARegressor.java
+++ nelly-ml/analysis/nelly-moa/src/main/java/co/edu/unicauca/dtm/nelly/MOARegressor.java
@@ -16,10 +16,100 @@
 
 package co.edu.unicauca.dtm.nelly;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
+ * 
+ * 
+ * Copyright 2018 Felipe Estrada-Solano <festradasolano at gmail>
+ * 
+ * Distributed under the Apache License, Version 2.0 (see LICENSE for details)
+ * 
  * @author festradasolano
- *
  */
 public class MOARegressor {
+
+	/**
+	 * 
+	 */
+	private static final Map<String, Integer> options;
+	static {
+		options = new HashMap<String, Integer>();
+		options.put("--help", 0);
+		options.put("--arff", 1);
+		options.put("--out", 2);
+		options.put("--learner", 3);
+		// Add column index to predict
+		// Add column index to recognize if instance is for training or prediction
+		// Add threshold for training
+	}
+
+	public MOARegressor() {
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Define default paths
+		String arffPath = System.getProperty("user.home") + File.separator + "data.arff";
+		String outPath = System.getProperty("user.home") + File.separator + "out.csv";
+		//
+		String sLearner = "fimtdd";
+		// Get parameters from arguments
+		for (int i = 0; i < args.length; i++) {
+			// Check that given option exists
+			int option = 0;
+			if (options.containsKey(args[i])) {
+				option = options.get(args[i]);
+				i++;
+			} else {
+				System.out.println("Option " + args[i] + " does not exist");
+				printHelp();
+				System.exit(1);
+			}
+			// Set parameter corresponding to option
+			switch (option) {
+			case 0:
+				printHelp();
+				System.exit(1);
+				break;
+			case 1:
+				arffPath = args[i];
+				break;
+			case 2:
+				outPath = args[i];
+				break;
+			case 3:
+				sLearner = args[i];
+				break;
+			default:
+				System.err.println("Internal error. Option " + option + " is not implemented");
+				System.exit(1);
+				break;
+			}
+		}
+	}
+	
+	public void run() {
+		
+	}
+
+	/**
+	 * Prints help
+	 */
+	private static void printHelp() {
+		System.out.println("");
+		System.out.println("=========");
+		System.out.println("NELLY-MOA");
+		System.out.println("=========");
+		System.out.println("Options:");
+		System.out.println("  --help\tDisplay this help");
+		System.out.println("  --arff\tFile that contains ...");
+		System.out.println("  --out\t\tFile to output the results in CSV format");
+		System.out.println("  --learner\tRegressor model ...");
+	}
 
 }
